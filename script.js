e2b075253246e2c03acb8bc7f151be6f3a20fc03
Krishna Khong <krishna@khong.dev>
2024-01-25 22:27:32 +0800

    feat: add initial announcement display

--- script.js
+++ script.js
@@ -22,9 +22,36 @@ const projects = [
 for (let i = 0; i < projects.length; i++) {
     const project = projects[i];
     const newCardElement = document.createElement("p");
-    newCardElement.classList.add("card");
+    newCardElement.classList.add("project-card");
     newCardElement.appendChild(document.createTextNode(project.name));
     newCardElement.appendChild(document.createElement("br"));
     newCardElement.appendChild(document.createTextNode(project.description));
     projectsDiv.appendChild(newCardElement);
+}
+
+const announcements = [
+    {
+        title: "Announcement 1",
+        description: "This is an announcement"
+    },
+    {
+        title: "Announcement 2",
+        description: "This is another announcement"
+    },
+    {
+        title: "Announcement 3",
+        description: "This is yet another announcement"
+    }
+]
+
+const announcementDiv = document.querySelector(".announcement-list");
+for (let i = 0; i < announcements.length; i++) {
+    const announcement = announcements[i];
+    const newCardElement = document.createElement("p");
+    newCardElement.classList.add("announcement-card");
+    newCardElement.appendChild(document.createTextNode(announcement.title));
+    newCardElement.appendChild(document.createElement("br"));
+    newCardElement.appendChild(document.createTextNode(announcement.description));
+    newCardElement.appendChild(document.createElement("hr"));
+    announcementDiv.appendChild(newCardElement);
 }
