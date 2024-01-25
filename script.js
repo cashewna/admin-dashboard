44b353e3de0f1d10fe6b8f3541192e5758cb60a4
Krishna Khong <krishna@khong.dev>
2024-01-25 23:07:48 +0800

    feat: initial trending cards

--- script.js
+++ script.js
@@ -55,3 +55,50 @@ for (let i = 0; i < announcements.length; i++) {
     newCardElement.appendChild(document.createElement("hr"));
     announcementDiv.appendChild(newCardElement);
 }
+
+const trending = [
+    {
+        username: "@tegan",
+        profilePicture: "https://res.cloudinary.com/demo/image/facebook/65646572251.jpg",
+        bio: "World Peace Builder"
+    },
+    {
+        username: "@morgan",
+        profilePicture: "https://res.cloudinary.com/demo/image/facebook/65646572251.jpg",
+        bio: "Super Cool Project"
+    },
+    {
+        username: "@kendall",
+        profilePicture: "https://res.cloudinary.com/demo/image/facebook/65646572251.jpg",
+        bio: "Life Changing App"
+    },
+    {
+        username: "@alex",
+        profilePicture: "https://res.cloudinary.com/demo/image/facebook/65646572251.jpg",
+        bio: "No Traffic Maker"
+    },
+]
+const trendingDiv = document.querySelector(".trending-list");
+for (let i = 0; i < trending.length; i++) {
+    const trend = trending[i];
+    const newCardElement = document.createElement("p");
+    newCardElement.classList.add("trending-card");
+    const left = document.createElement("div");
+    left.classList.add("left");
+    
+    const img = document.createElement("img");
+    img.src = trend.profilePicture;
+    left.appendChild(img);
+    
+    const right = document.createElement("div");
+    right.classList.add("right");
+    const username = document.createElement("p");
+    username.innerText = trend.username;
+    right.appendChild(username);
+    const bio = document.createElement("p");
+    bio.innerText = trend.bio;
+    right.appendChild(bio);
+    newCardElement.appendChild(left);
+    newCardElement.appendChild(right);
+    trendingDiv.appendChild(newCardElement);
+}
