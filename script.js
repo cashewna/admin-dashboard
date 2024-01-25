1897c1df441a919725070e9a8879f70903d2eed8
Krishna Khong <krishna@khong.dev>
2024-01-25 21:40:34 +0800

    fix: make project list grow dynamically

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ const username_class = document.querySelectorAll(".username");
 for (let i = 0; i < username_class.length; i++) {
     username_class[i].innerHTML = username;
 }
+
+const projectsDiv = document.querySelector(".project-list");
+const projects = [
+    {
+        name: "Project 1",
+        description: "This is a project",
+        link: "https://www.google.com"
+    },
+    {
+        name: "Project 2",
+        description: "This is another project",
+        link: "https://notes.khong.dev"
+    }
+]
+
+for (let i = 0; i < projects.length; i++) {
+    const project = projects[i];
+    const newCardElement = document.createElement("p");
+    newCardElement.classList.add("card");
+    newCardElement.appendChild(document.createTextNode(project.name));
+    newCardElement.appendChild(document.createElement("br"));
+    newCardElement.appendChild(document.createTextNode(project.description));
+    projectsDiv.appendChild(newCardElement);
+}
